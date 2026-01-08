0b011328bdd20e37e24c38c5236d1ce281f2f267
manabumantappu <azishachigo@gmail.com>
2026-01-08 10:25:06 +0700

    Update GameOverScene.js

--- src/scenes/GameOverScene.js
+++ src/scenes/GameOverScene.js
@@ -4,29 +4,37 @@ export default class GameOverScene extends Phaser.Scene {
   }
 
   init(data) {
-    this.finalScore = data.score;
+    this.score = data.score;
   }
 
   create() {
     const { width, height } = this.scale;
 
-    this.add.text(width / 2, height / 2 - 40, "GAME OVER", {
+    const bestScore = Math.max(
+      this.score,
+      localStorage.getItem("bestScore") || 0
+    );
+    localStorage.setItem("bestScore", bestScore);
+
+    this.add.text(width / 2, height / 2 - 60, "GAME OVER", {
       fontSize: "32px",
       color: "#ff5555"
     }).setOrigin(0.5);
 
-    this.add.text(width / 2, height / 2, `Score: ${this.finalScore}`, {
+    this.add.text(width / 2, height / 2 - 10,
+      `Score: ${this.score}\nBest: ${bestScore}`, {
       fontSize: "18px",
+      align: "center",
       color: "#ffffff"
     }).setOrigin(0.5);
 
-    this.add.text(width / 2, height / 2 + 60, "PRESS SPACE TO RESTART", {
+    this.add.text(width / 2, height / 2 + 60,
+      "TAP or PRESS SPACE", {
       fontSize: "14px",
       color: "#00ffcc"
     }).setOrigin(0.5);
 
-    this.input.keyboard.once("keydown-SPACE", () => {
-      this.scene.start("MenuScene");
-    });
+    this.input.once("pointerdown", () => this.scene.start("MenuScene"));
+    this.input.keyboard.once("keydown-SPACE", () => this.scene.start("MenuScene"));
   }
 }
