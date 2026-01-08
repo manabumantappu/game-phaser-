920850f2b9aa4b499e1a82a2c549446aa4d3fbc5
manabumantappu <azishachigo@gmail.com>
2026-01-08 16:02:20 +0700

    Update GameScene.js

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,129 +1,111 @@
+import { LEVELS } from "../data/levels.js";
+import VirtualJoystick from "../ui/VirtualJoystick.js";
+
 export default class GameScene extends Phaser.Scene {
   constructor() {
     super({ key: "GameScene" });
   }
 
-  // 🔑 TERIMA DATA DARI MENU
   init(data) {
-    this.level = data.level ?? 1;
-    this.score = data.score ?? 0;
-    this.maxLevel = 3;
+    this.levelIndex = data.level ?? 0;
   }
 
   create() {
+    this.levelData = LEVELS[this.levelIndex];
+
     this.cursors = this.input.keyboard.createCursorKeys();
-    this.loadLevel(this.level);
+
+    // SOUNDS
+    this.sfxEat = this.sound.add("eat");
+    this.sfxWin = this.sound.add("win");
+
+    this.createWorld();
+    this.createPlayer();
+    this.createJoystick();
   }
 
-  clearLevel() {
-    this.children.removeAll();
-    this.physics.world.colliders.destroy();
-  }
-
-  loadLevel(level) {
-    this.clearLevel();
-
-    this.collects = this.physics.add.group();
+  createWorld() {
     this.walls = this.physics.add.staticGroup();
+    this.pellets = this.physics.add.group();
 
-    // PLAYER
-    this.player = this.add.rectangle(40, 40, 28, 28, 0x3498db);
-    this.physics.add.existing(this.player);
-    this.player.body.setCollideWorldBounds(true);
+    // WALLS
+    this.levelData.walls.forEach(w => {
+      const wall = this.walls.create(w.x, w.y, "wall");
+      wall.setDisplaySize(w.w, w.h);
+      wall.refreshBody();
+    });
 
-    if (level === 1) {
-      this.createCollects([
-        { x: 120, y: 200 },
-        { x: 300, y: 400 }
-      ]);
-      this.createGoal(420, 580);
-    }
+    // PELLETS
+    this.levelData.pellets.forEach(p => {
+      this.pellets.create(p.x, p.y, "pellet");
+    });
 
-    if (level === 2) {
-      this.createCollects([
-        { x: 80, y: 300 },
-        { x: 240, y: 200 },
-        { x: 380, y: 450 }
-      ]);
+    // GOAL
+    this.goal = this.physics.add.staticImage(
+      this.levelData.goal.x,
+      this.levelData.goal.y,
+      "goal"
+    );
+    this.goal.setVisible(false);
+  }
 
-      this.createRedWalls([
-        { x: 240, y: 320, w: 320, h: 20 }
-      ]);
+  createPlayer() {
+    this.player = this.physics.add.sprite(
+      this.levelData.player.x,
+      this.levelData.player.y,
+      "player"
+    );
 
-      this.createGoal(420, 60);
-    }
-
-    if (level === 3) {
-      this.createCollects([
-        { x: 60, y: 580 },
-        { x: 420, y: 100 }
-      ]);
-
-      this.createRedWalls([
-        { x: 240, y: 80, w: 400, h: 20 },
-        { x: 240, y: 560, w: 400, h: 20 },
-        { x: 120, y: 320, w: 20, h: 400 },
-        { x: 360, y: 320, w: 20, h: 400 }
-      ]);
-
-      this.createGoal(240, 320);
-    }
+    this.player.setCollideWorldBounds(true);
 
     this.physics.add.collider(this.player, this.walls);
 
-    this.physics.add.overlap(this.player, this.collects, (_, c) => {
-      c.destroy();
+    this.physics.add.overlap(this.player, this.pellets, (_, p) => {
+      p.destroy();
+      this.sfxEat.play();
+
+      if (this.pellets.countActive(true) === 0) {
+        this.goal.setVisible(true);
+      }
     });
 
     this.physics.add.overlap(this.player, this.goal, () => {
-      if (this.collects.countActive(true) === 0) {
-        this.nextLevel();
-      }
+      this.sfxWin.play();
+      this.nextLevel();
     });
+  }
+
+  createJoystick() {
+    this.joystick = new VirtualJoystick(this);
   }
 
   nextLevel() {
-    this.level++;
-
-    if (this.level > this.maxLevel) {
-      this.scene.start("GameOverScene", {
-        score: this.score
-      });
+    if (this.levelIndex + 1 >= LEVELS.length) {
+      this.scene.start("GameOverScene");
     } else {
-      this.loadLevel(this.level);
+      this.scene.start("GameScene", {
+        level: this.levelIndex + 1
+      });
     }
   }
 
-  createCollects(list) {
-    list.forEach(p => {
-      const c = this.add.rectangle(p.x, p.y, 20, 20, 0xf1c40f);
-      this.physics.add.existing(c);
-      this.collects.add(c);
-    });
-  }
-
-  createGoal(x, y) {
-    this.goal = this.add.rectangle(x, y, 26, 26, 0x2ecc71);
-    this.physics.add.existing(this.goal, true);
-  }
-
-  createRedWalls(list) {
-    list.forEach(w => {
-      const wall = this.add.rectangle(w.x, w.y, w.w, w.h, 0xe74c3c);
-      this.physics.add.existing(wall, true);
-      this.walls.add(wall);
-    });
-  }
-
   update() {
-    const speed = 200;
-    const body = this.player.body;
+    const speed = 160;
+    let vx = 0;
+    let vy = 0;
 
-    body.setVelocity(0);
+    if (this.cursors.left.isDown) vx = -speed;
+    else if (this.cursors.right.isDown) vx = speed;
 
-    if (this.cursors.left.isDown) body.setVelocityX(-speed);
-    if (this.cursors.right.isDown) body.setVelocityX(speed);
-    if (this.cursors.up.isDown) body.setVelocityY(-speed);
-    if (this.cursors.down.isDown) body.setVelocityY(speed);
+    if (this.cursors.up.isDown) vy = -speed;
+    else if (this.cursors.down.isDown) vy = speed;
+
+    // MOBILE JOYSTICK
+    if (this.joystick) {
+      vx = this.joystick.forceX * speed;
+      vy = this.joystick.forceY * speed;
+    }
+
+    this.player.setVelocity(vx, vy);
   }
 }
