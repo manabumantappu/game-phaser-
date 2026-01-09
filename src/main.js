db855f1ab5a778662f18014f11a176204ebce4a0
manabumantappu <azishachigo@gmail.com>
2026-01-09 14:53:41 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,20 +1,15 @@
 import BootScene from "./scenes/BootScene.js";
 import MenuScene from "./scenes/MenuScene.js";
 import GameScene from "./scenes/GameScene.js";
-import GameOverScene from "./scenes/GameOverScene.js";
 
 new Phaser.Game({
   type: Phaser.AUTO,
-  width: 416,
-  height: 416,
+  width: 480,
+  height: 640,
   backgroundColor: "#000000",
-  physics: {
-    default: "arcade",
-    arcade: { debug: false }
-  },
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH
   },
-  scene: [BootScene, MenuScene, GameScene, GameOverScene]
+  scene: [BootScene, MenuScene, GameScene]
 });
