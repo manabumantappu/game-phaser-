fd7675c2a4d22dc01bc34a51daba41f2ef798af2
manabumantappu <azishachigo@gmail.com>
2026-01-09 15:56:46 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import BootScene from "./scenes/BootScene.js";
 import MenuScene from "./scenes/MenuScene.js";
 import GameScene from "./scenes/GameScene.js";
 
+const config = {
+  type: Phaser.AUTO,
+  width: 480,
+  height: 640,
+  backgroundColor: "#000000",
+  parent: "game",
+  scene: [BootScene, MenuScene, GameScene]
+};
+
 new Phaser.Game({
   type: Phaser.AUTO,
   width: 480,
