4852e82e1fabecacd4b93db97366cd715927a8b3
manabumantappu <azishachigo@gmail.com>
2026-01-09 16:36:58 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,19 +5,13 @@ import GameScene from "./scenes/GameScene.js";
 const config = {
   type: Phaser.AUTO,
   width: 480,
-  height: 800,
+  height: 640,
   backgroundColor: "#000000",
+  parent: "game",
 
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH
-  },
-
-  physics: {
-    default: "arcade",
-    arcade: {
-      debug: false
-    }
   },
 
   input: {
